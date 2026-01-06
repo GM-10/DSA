ef6d632b755285c4bf7c4406bdf9232564ef4027
Grishma Makwana <grishmamakwana1@gmail.com>
2026-01-06 10:35:46 +0530

    Inserting in a linked list

--- Linked List/Inserting a node in end.py
+++ Linked List/Inserting a node in end.py
@@ -6,11 +6,16 @@ class Node:
 
 if __name__ == "__main__":
     arr=[1,2,3,4,5]
+    num=0
     head=Node(arr[0])
     current=head
     for i in range(1,len(arr)):
         current.next=Node(arr[i])
         current=current.next
+    
+    new_node=Node(num)
+    current.next=new_node
+    
     temp = head
     while temp:
         print(temp.data, end=" -> ")
